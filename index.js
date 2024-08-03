c36aa50a8e210c0269a621fed430d7f82a9d9c94
KritirajChakraborty <kritiraj.exams2021@gmail.com>
2024-08-03 17:21:00 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import userRouter from "./routes/user.route.js";
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on 3000...");
+});
+
+app.use("/api/user", userRouter);
