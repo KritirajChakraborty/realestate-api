6b871a210bfc7989e7418e85c18d57cfeffc0bc9
KritirajChakraborty <kritiraj.exams2021@gmail.com>
2024-09-02 22:19:27 +0530

    complete get listing route api

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -3,6 +3,7 @@ import { verifyUser } from "../utils/verifyUser.js";
 import {
   createListing,
   deleteListing,
+  getListing,
   updateListing,
 } from "../controllers/listing.controller.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyUser, createListing);
 router.delete("/delete/:id", verifyUser, deleteListing);
 router.post("/update/:id", verifyUser, updateListing);
+router.get("/get/:id", getListing);
 
 export default router;
