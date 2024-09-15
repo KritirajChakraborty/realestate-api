10e05764cfc7cb9f9a88b901bf61bc3ddb35efb2
KritirajChakraborty <kritiraj.exams2021@gmail.com>
2024-09-15 23:22:28 +0530

    cors setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
@@ -21,6 +22,25 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://realestate-client-qdthzgtut-kritirajs-projects.vercel.app',
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (e.g., mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, //(for authentication)
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
