207eef1492c42fe80cc501c9f24292943dde8a21
KritirajChakraborty <kritiraj.exams2021@gmail.com>
2024-09-15 22:51:31 +0530

    prod

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on 3000...');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server is LIVE...');
 });
 
 app.use('/api/user', userRouter);
